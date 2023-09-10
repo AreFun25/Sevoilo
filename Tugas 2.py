6fcfa9da220253d8e2ea290f8a57321ff4ac5f86
AreFun25 <141648095+AreFun25@users.noreply.github.com>
2023-09-10 15:29:40 +0700

    Add files via upload

---
+++ Tugas 2.py
@@ -0,0 +1,27 @@
+# Mencetak ke layar tulisan sekaligus varibael
+name = "Contoh"
+age = 30
+print("Hello", name)
+print("namamu adalah", age, "tahun")
+
+# Deklarasi sekaligus inisialisasi variabel baru
+import math
+number = 36
+x = 4
+print( number + x)
+
+# menghitung akar 2 dari sebuah angka
+square_root = math.sqrt(number)
+print("Akar dari", number, "adalah", square_root)
+
+# menghitung pangkat
+exponent = 3
+result = x ** exponent
+print(x, "dipangkatkan", exponent, "adalah", result)
+
+# Inputan keyboard 
+
+user_input = input("Enter a number: ")
+
+number = float(user_input)
+
